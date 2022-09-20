21478b1d55bd009cd6d8a595947add10c5f2f89c
KarthickM1112 <karthickmariyappan1999@gmail.com>
2022-09-20 10:36:28 +0530

    function

--- CheckContains.java
+++ CheckContains.java
@@ -2,7 +2,7 @@ package StringMethods;
 
 public class CheckContains {
 	public static void main(String[]args) {
-		String sentance="He playing cricket 5";
+		String sentance="He playing cricket 10";
 		System.out.println(sentance.contains("5"));
 	}
 
